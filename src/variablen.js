0bfdfd7233305dcc7df97e8138cc12b5fe4953bd
JH <jasper.haag@icloud.com>
2024-01-09 12:24:04 +0100

    add sometypes

--- src/variablen.js
+++ src/variablen.js
@@ -2,4 +2,12 @@ console.log("hi");
 
 let firstName = "Max";
 let lastName = "Mustermann";
-console.log(firstName + " " +lastName);
+console.log(firstName + " " +lastName +" "+typeof lastName);
+lastName = 1;
+
+console.log(firstName + " " +lastName +" "+typeof lastName);
+lastName = true;
+
+console.log(firstName + " " +lastName +" "+typeof lastName);
+lastName = 1.0
+console.log(firstName + " " +lastName +" "+typeof lastName);
