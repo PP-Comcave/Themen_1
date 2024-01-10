4c999bc872956bf98cec9c9eebaef25c2e23c2a8
JH <jasper.haag@icloud.com>
2024-01-10 10:38:40 +0100

    add output

--- src/prompt.js
+++ src/prompt.js
@@ -1,4 +1,6 @@
 const prompt = require('prompt-sync')({sigint: true});
 let firstName = prompt("vorname  ");
-
-console.log(firstName);
+console.log("vorname ist gespeichert!")
+let familyName = prompt("nachname  ");
+console.log("nachname ist gespeichert!")
+console.log(firstName,familyName);
