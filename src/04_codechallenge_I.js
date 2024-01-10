069e95cf5fd1fbd075028a1f235e2cd81108e626
JH <jasper.haag@icloud.com>
2024-01-10 12:24:38 +0100

    add warheit

--- src/04_codechallenge_I.js
+++ src/04_codechallenge_I.js
@@ -15,7 +15,16 @@ console.log("hello");
 let zahl1 = Number(prompt("zahl 1 = "));
 
 let zahl2 = Number(prompt("zahl 2 = "));
-
 let summe = Number(zahl1 + zahl2);
+
+
+if (isNaN(zahl1) || isNaN(zahl2)) {
+   console.log("number1")
+}else{
+    console.log("number2")
+}
+
+
+
 
 console.log("das ergebnis von "+ zahl1 +" + " + zahl2 + " lautet " + summe);
