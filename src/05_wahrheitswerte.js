8d7f2f2ab648a5743ed2c8fbd92f0f20b029ec6d
JH <jasper.haag@icloud.com>
2024-01-10 12:46:30 +0100

    jetzt mit jahr vom system

--- src/05_wahrheitswerte.js
+++ src/05_wahrheitswerte.js
@@ -6,10 +6,12 @@ let ageJohn, ageMark;
 const birthYearJohn = 2000;
 const birthYearMark = 1990;
 
-let year = 2024;
+let date = new Date();
 //Berechnung Alter
+let year = date.getFullYear();
 ageJohn = year - birthYearJohn;
 ageMark = year - birthYearMark;
 
 console.log("ageJohn " + ageJohn);
 console.log("ageMark " + ageMark);
+console.log("datum " + year);
